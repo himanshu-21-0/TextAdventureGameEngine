3c8c81fd96ac1f6b8dbbbd5fcde84c4935700a73
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-17 20:26:11 +0530

    Add Item Storage to Room Class using List

--- src/com/textadventure/model/Room.java
+++ src/com/textadventure/model/Room.java
@@ -2,12 +2,14 @@ package com.textadventure.model;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Room {
     private String name;
     private String description;
-
     private Map<String, String> exits;
+    private List<Item> items;
 
     public Room(String name, String description) {
         this.name = name;
