b82cecc95a9d9e20c52c46a086db93dde317d67d
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-18 23:06:24 +0530

    Declaring the inventory Attribute

--- src/com/textadventure/model/Player.java
+++ src/com/textadventure/model/Player.java
@@ -1,6 +1,10 @@
 package com.textadventure.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Player {
     private String currentRoomName;
     private String currentRoom;
+    private List<Item> inventory;
 }
