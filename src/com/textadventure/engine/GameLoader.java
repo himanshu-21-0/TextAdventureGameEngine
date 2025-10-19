22e37b3b49cbd8fe6d03c63dff4afd496cc64513
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-19 21:49:57 +0530

    Changes to GameLoader.java

--- src/com/textadventure/engine/GameLoader.java
+++ src/com/textadventure/engine/GameLoader.java
@@ -1,8 +1,5 @@
 package com.textadventure.engine;
 
-import com.textadventure.model.Room;
-import com.textadventure.model.Item;
-
 public class GameLoader {
     public GameLoader() {
         System.out.println("GameLoader initialized.");
