5e6b6f0326c4365e4d6013ff60f6314b5796a0e7
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-19 17:42:20 +0530

    Creating the GameLoader Class Structure

--- src/com/textadventure/engine/GameLoader.java
+++ src/com/textadventure/engine/GameLoader.java
@@ -1,5 +1,10 @@
 package com.textadventure.engine;
 
+import com.textadventure.model.Room;
+import com.textadventure.model.Item;
+
 public class GameLoader {
-    
+    public GameLoader() {
+        System.out.println("GameLoader initialized.");
+    }
 }
