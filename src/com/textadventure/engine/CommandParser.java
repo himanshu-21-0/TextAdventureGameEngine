567f8530d7cbf45441f2b0c2bcdd61a89f22dc32
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-24 22:02:13 +0530

    Creating the CommandParser Class

--- src/com/textadventure/engine/CommandParser.java
+++ src/com/textadventure/engine/CommandParser.java
@@ -3,6 +3,6 @@ package com.textadventure.engine;
 public class CommandParser {
 
     public CommandParser() {
-        System.out.println("CommandParser Initialized");
+        System.out.println("CommandParser initialized");
     }
 }
