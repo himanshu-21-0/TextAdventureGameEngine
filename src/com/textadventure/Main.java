03ebef95d73b50231f09257e434fb47b42150186
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-25 19:01:42 +0530

    Commenting out Scanner Class for now

--- src/com/textadventure/Main.java
+++ src/com/textadventure/Main.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Main {
 
