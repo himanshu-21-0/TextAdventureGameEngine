f7590b7c040d9fe56b211c390398fbe672782161
Himanshu Singh <himanshu124singh19@gmail.com>
2025-10-31 23:41:54 +0530

    Gather Game State Data for Save Feature

--- src/com/textadventure/game/Game.java
+++ src/com/textadventure/game/Game.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class Game {
