a1b5412bced38a350f658faa8b371ba5f937b556
Himanshu Singh <himanshu124singh19@gmail.com>
2025-11-01 15:30:58 +0530

    Creating a SaveState Class

---
+++ src/com/textadventure/utils/SaveState.java
@@ -0,0 +1,31 @@
+package com.textadventure.utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class SaveState {
+    private String playerLocation;
+    private List<String> playerInventory;
+    private Map<String, List<String>> roomItemStates;
+
+    public SaveState(String playerLocation, List<String> playerInventory, Map<String, List<String>> roomItemStates) {
+        this.playerLocation = playerLocation;
+        this.playerInventory = playerInventory;
+        this.roomItemStates = roomItemStates;
+    }
+
+    public String getPlayerLocation() {
+        return playerLocation;
+    }
+
+    public List<String> getPlayerInventory() {
+        return playerInventory;
+    }
+
+    public Map<String, List<String>> getRoomItemStates() {
+        return roomItemStates;
+    }
+
+    public SaveState() {
+    }
+}
