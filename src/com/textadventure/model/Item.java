16a2bbb784bfe9c702c8e86e427838aa94bee7da
Himanshu Singh <himanshu124singh19@gmail.com>
2025-11-07 23:26:02 +0530

    Define Item Usability Data Structure

--- src/com/textadventure/model/Item.java
+++ src/com/textadventure/model/Item.java
@@ -1,8 +1,12 @@
 package com.textadventure.model;
 
+import java.util.List;
+
 public class Item {
     private String name;
     private String description;
+    private List<String> items;
+    private Usability usability;
 
     public Item(String name, String description) {
         if (name == null || name.trim().isEmpty())
@@ -10,9 +14,11 @@ public class Item {
 
         if (description == null)
             throw new IllegalArgumentException("Item description cannot be null.");
-        
+
         this.name = name;
         this.description = description;
+        this.usability = null;
+        this.items = null;
     }
 
     public String getName() {
@@ -22,4 +28,57 @@ public class Item {
     public String getDescription() {
         return this.description;
     }
+
+    public List<String> getItems() {
+        return (items == null) ? List.of() : items;
+    }
+
+    public Usability getUsability() {
+        return usability;
+    }
+
+    public static class Usability {
+        private String target;
+        private String effectDescription;
+        private boolean consumesItem;
+        private String unlocksExit;
+        private String removesTarget;
+        private String addsTarget;
+        private String changesRoomDescriptionTo;
+        private String addsItemToInventory;
+
+        public String getTarget() {
+            return target;
+        }
+
+        public String getEffectDescription() {
+            return effectDescription;
+        }
+
+        public boolean isConsumesItem() {
+            return consumesItem;
+        }
+
+        public String getUnlocksExit() {
+            return unlocksExit;
+        }
+
+        public String getRemovesTarget() {
+            return removesTarget;
+        }
+
+        public String getAddsTarget() {
+            return addsTarget;
+        }
+
+        public String getChangesRoomDescriptionTo() {
+            return changesRoomDescriptionTo;
+        }
+
+        public String getAddsItemToInventory() {
+            return addsItemToInventory;
+        }
+
+    }
+
 }
